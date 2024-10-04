5df91bbf7147af6d1cd82d49c61ce5fe8258a13a
Anuoluwapo Balogun <83256563+analyticsengineer@users.noreply.github.com>
2024-10-04 23:36:57 +0100

    Update View_dataset.py
    update

--- ~/app/pages/View_dataset.py
+++ ~/app/pages/View_dataset.py
@@ -3,7 +3,7 @@ from google.oauth2 import service_account
 from google.cloud import bigquery
 
 # Create API client.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
-client = bigquery.Client(credentials=credentials)
+#credentials = service_account.Credentials.from_service_account_info(
+    #st.secrets["gcp_service_account"]
+#)
+#client = bigquery.Client(credentials=credentials)

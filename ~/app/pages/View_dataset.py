b042aab674c4d23ddd87698bfe01e64650fc4690
Anuoluwapo Balogun <83256563+analyticsengineer@users.noreply.github.com>
2024-10-04 23:47:38 +0100

    Update View_dataset.py
    update

--- ~/app/pages/View_dataset.py
+++ ~/app/pages/View_dataset.py
@@ -1,9 +1,8 @@
-#import streamlit as st
-#from google.oauth2 import service_account
-#from google.cloud import bigquery
+import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import bigquery
 
-# Create API client.
-#credentials = service_account.Credentials.from_service_account_info(
-    #st.secrets["gcp_service_account"]
-#)
-#client = bigquery.Client(credentials=credentials)
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials)

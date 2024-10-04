7ca238d311d94adf536ee2ea6b1bf654a1f7d583
Anuoluwapo Balogun <83256563+analyticsengineer@users.noreply.github.com>
2024-10-04 23:18:57 +0100

    Update Homepage.py
    updated

--- ~/app/Homepage.py
+++ ~/app/Homepage.py
@@ -9,12 +9,12 @@ col1, col2 = st.columns(2)
 
 col1.header("DataFest 2024 Hackathon")
 col1.write("Challenges Faced by Students in WAEC and JAMB Exams in Africa")
-col1.write("Our simple data visualization app makes it easy to create beautiful and informative data visualizations.")
+col1.write("WAEC (West African Examinations Council) and JAMB (Joint Admissions and Matriculation Board) are critical examinations for students in West Africa, particularly Nigeria, as they determine access to secondary and tertiary education. Despite their importance, students face several challenges when preparing for and taking these exams, ranging from socioeconomic barriers to educational gaps.")
 
-title_1 =  '<p style="font-family:sans-serif; color:Grey;">This Web App helps you visualize your data,</p>'
+title_1 =  '<p style="font-family:sans-serif; color:Grey;">This data hackathon aims to explore these challenges using data-driven insights, providing solutions and recommendations to improve the exam processes and outcomes for students.</p>'
 col1.markdown(title_1, unsafe_allow_html=True)
-title_2 =  '<p style="font-family:sans-serif; color:Grey;">in a simple and elegant way.</p>'
-col1.markdown(title_2, unsafe_allow_html=True)
+#title_2 =  '<p style="font-family:sans-serif; color:Grey;">in a simple and elegant way.</p>'
+#col1.markdown(title_2, unsafe_allow_html=True)
 col2.image(image)
 
 # Setting menu visibility

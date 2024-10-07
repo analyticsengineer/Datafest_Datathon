ba4d65a4a96e4ffcd62804d35457f4fc6b7286d4
Anuoluwapo Balogun <83256563+analyticsengineer@users.noreply.github.com>
2024-10-07 19:57:43 +0100

    Update View_dataset.py
    added code

--- ~/app/pages/View_dataset.py
+++ ~/app/pages/View_dataset.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
+url = "https://docs.google.com/spreadsheets/d/1bdywDzhqfDHut36grVDXjEkxYsm7YX_66sLYOyHh7w4/edit?usp=sharing"
 
 conn = st.connection("gsheets", type=GSheetsConnection)
 

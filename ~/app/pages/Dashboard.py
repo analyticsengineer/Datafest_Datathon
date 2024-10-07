baf2c2522fb692fe655a5cad1c515b6a40e8cc5e
Anuoluwapo Balogun <83256563+analyticsengineer@users.noreply.github.com>
2024-10-07 20:37:28 +0100

    Update Dashboard.py
    added code

--- ~/app/pages/Dashboard.py
+++ ~/app/pages/Dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Set the title of the app
-st.title("Embed Power BI Report")
+st.title("Dashboard Report")
 
 # Embed Power BI report using an iframe
 powerbi_url = "https://app.powerbi.com/view?r=eyJrIjoiYWNlNGVmYmItNWMyZS00M2FiLTk3YTUtNGE5NjEzY2Y5MzUwIiwidCI6ImRmODY3OWNkLWE4MGUtNDVkOC05OWFjLWM4M2VkN2ZmOTVhMCJ9"
@@ -11,5 +11,5 @@ iframe = f"""
         frameborder="0" 
         allowFullScreen="true"></iframe>
 """
-st.components.v1.html(iframe, height=600)
+st.components.v1.html(iframe, height=1000)
 

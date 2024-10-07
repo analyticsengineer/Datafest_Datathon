8311ecaa09e196e76d3cf1efba798bdc148cb613
Anuoluwapo Balogun <83256563+analyticsengineer@users.noreply.github.com>
2024-10-07 20:23:21 +0100

    Update View_dataset.py
    added code

--- ~/app/pages/View_dataset.py
+++ ~/app/pages/View_dataset.py
@@ -1,7 +1,18 @@
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
 
-# Create a connection object.
-conn = st.connection("gsheets", type=GSheetsConnection)
+# Set the title of the app
+st.title("Embed Google Sheet")
 
-df = conn.read()
+# Embed Google Sheet using an iframe
+# Make sure to set the URL to the published Google Sheet
+google_sheet_url = "https://docs.google.com/spreadsheets/d/1bdywDzhqfDHut36grVDXjEkxYsm7YX_66sLYOyHh7w4/edit?usp=sharing"
+iframe = f"""
+<iframe 
+    src="{google_sheet_url}" 
+    width="800" 
+    height="600" 
+    frameborder="0" 
+    allowfullscreen>
+</iframe>
+"""
+st.components.v1.html(iframe, height=600)

6169742df0ad52cb10460ca1aaecb44dc5f2aa5c
Anuoluwapo Balogun <83256563+analyticsengineer@users.noreply.github.com>
2024-10-08 23:04:48 +0100

    Update View_dataset.py
    added code

--- ~/app/pages/View_dataset.py
+++ ~/app/pages/View_dataset.py
@@ -1,20 +1,26 @@
 import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import bigquery
 
-# Set the title of the app
-st.title("Student Response")
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials)
 
-# Embed Google Sheet using an iframe
-# Use the published URL of the Google Sheet
-google_sheet_url = "https://docs.google.com/spreadsheets/d/1bdywDzhqfDHut36grVDXjEkxYsm7YX_66sLYOyHh7w4/edit?usp=sharing"
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    query_job = client.query(query)
+    rows_raw = query_job.result()
+    # Convert to list of dicts. Required for st.cache_data to hash the return value.
+    rows = [dict(row) for row in rows_raw]
+    return rows
 
-iframe = f"""
-<iframe 
-    src="{google_sheet_url}" 
-    width="800" 
-    height="600" 
-    frameborder="0" 
-    allowfullscreen>
-</iframe>
-"""
+# Query to fetch data from your BigQuery table
+rows = run_query("SELECT * FROM `datafest-hackathon.datafest_2024.googlesheet` LIMIT 100")
 
-st.components.v1.html(iframe, height=1000)
+# Display the results as a table
+st.write("Data from googlesheet table:")
+st.dataframe(rows)  # or use st.table(rows) for a static table
